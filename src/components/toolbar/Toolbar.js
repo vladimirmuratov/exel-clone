9611322c4cd251b0dc2372036d7f01fae7adeffb
vladimir <vladimirmuratov@mail.ru>
2022-10-23 16:10:01 +0300

    Finish base framework functional

---
+++ src/components/toolbar/Toolbar.js
@@ -0,0 +1,50 @@
+import {ExelComponent} from '@core/ExelComponent'
+
+export class Toolbar extends ExelComponent {
+  static className = 'exel__toolbar'
+  constructor($root) {
+    super($root, {
+      name: 'Toolbar',
+      listeners: ['click']
+    })
+  }
+
+  onClick(event) {
+    console.log('Toolbar: onClick', event.target)
+  }
+
+  toHTML() {
+    return `
+      <div class="button">
+                    <span class="material-icons">
+                    format_align_left
+                    </span>
+            </div>
+            <div class="button">
+                    <span class="material-icons">
+                    format_align_center
+                    </span>
+            </div>
+            <div class="button">
+                    <span class="material-icons">
+                    format_align_right
+                    </span>
+            </div>
+            <div class="button">
+                    <span class="material-icons">
+                    format_bold
+                    </span>
+            </div>
+            <div class="button">
+                    <span class="material-icons">
+                    format_italic
+                    </span>
+            </div>
+            <div class="button">
+                    <span class="material-icons">
+                    format_underlined
+                    </span>
+            </div>
+    `
+  }
+}
