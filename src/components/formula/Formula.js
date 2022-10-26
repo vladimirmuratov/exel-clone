6b7d5d0cfa4f05f94c62c46834657192071b3869
vladimir <vladimirmuratov@mail.ru>
2022-10-26 14:00:21 +0300

    Finish table logic

--- src/components/formula/Formula.js
+++ src/components/formula/Formula.js
@@ -1,27 +1,43 @@
 import {ExelComponent} from '@core/ExelComponent'
+import {$} from '@core/dom';
 
 export class Formula extends ExelComponent {
   static className = 'exel__formula'
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
       name: 'Formula',
-      listeners: ['input', 'click']
+      listeners: ['input', 'keydown'],
+      ...options
+    })
+  }
+
+  init() {
+    super.init()
+    this.$formula = this.$root.find('#formula')
+    this.$on('table:select', $cell => {
+      this.$formula.text($cell.text())
+    })
+    this.$on('table:input', $cell => {
+      this.$formula.text($cell.text())
     })
   }
 
   toHTML() {
     return `
        <div class="info">fx</div>
-       <div class="input" contenteditable spellcheck="false"></div>
+       <div id="formula" class="input" contenteditable spellcheck="false"></div>
     `
   }
 
   onInput(event) {
-    console.log(this.$root)
-    console.log('Formula: onInput', event.target.textContent)
+    this.$dispatch('formula:input', $(event.target).text())
   }
 
-  onClick(event) {
-    console.log('onClick')
+  onKeydown(event) {
+    const keys = ['Enter', 'Tab']
+    if (keys.includes(event.key)) {
+      event.preventDefault()
+      this.$dispatch('formula:done')
+    }
   }
 }
